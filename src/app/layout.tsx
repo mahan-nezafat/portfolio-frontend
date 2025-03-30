c1603105728643e7c6486b6a1b35d2e138fbead9
mahan nezafat <nezafatmahan@gmail.com>
2025-03-30 22:09:08 +0330

    update layout and page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
+// const geistSans = Geist({
+//     variable: "--font-geist-sans",
+//     subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//     variable: "--font-geist-mono",
+//     subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
     title: "portfolio",
@@ -25,7 +25,7 @@ export default function RootLayout({
     return (
         <html lang="fa">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+               
             >
                 {children}
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,39 @@
 // import Image from "next/image";
-
+"use client";
 import { Blogs } from "@/components/blogs";
+import ConsultationForm from "@/components/consultation-form";
 import { Hero } from "@/components/hero";
 import { Nav } from "@/components/nav";
 import { Projects } from "@/components/projects";
 import { Services } from "@/components/services";
 import { Steps } from "@/components/steps";
+import { useState } from "react";
 
 export default function Home() {
+    const [isActive, setActive] = useState<boolean>(false);
+
     return (
-        <>
-            <header>
+        <div >
+            {isActive && (
+                <div id="overlay" className=" w-[100%] max-md:w-full h-[100vh] fixed z-100">
+                    <div className="blur-none w-[30vw] max-md:w-[90vw] max-md:h-[75vh] h-[80vh] bg-(--neutral)
+                     border-[1px] border-(--secondary) rounded-lg fixed translate-x-[-6%] translate-y-5">
+                        <ConsultationForm />
+                    </div>
+                </div>
+            )}
+
+
+            <header className={` ${isActive && "blur-xs"}`} >
                 <Nav />
-                <Hero />
+                <Hero isActive={isActive} setActive={setActive} />
             </header>
-            <main>
-               
+            <main className={` ${isActive && "blur-xs"}`}>
                 <Projects />
                 <Services />
-                <Steps/>
-                <Blogs/>
+                <Steps />
+                <Blogs />
             </main>
-        </>
+        </div>
     );
 }
